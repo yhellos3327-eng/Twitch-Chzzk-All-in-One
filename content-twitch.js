8d9dbcd4ba56bfbc9a43a5ae4fd2449c38ad8895
yhellos3327-eng <yhellos3327@gmail.com>
2026-02-05 22:36:38 +0900

    fix

--- content-twitch.js
+++ content-twitch.js
@@ -330,7 +330,7 @@
 
     const button = document.createElement('button');
     button.id = 'stream-bypass-btn';
-    button.innerHTML = '🎬 1080p 플레이어';
+    button.innerHTML = '<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align: middle; margin-right: 4px;"><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"/></svg> 1080p 플레이어';
     button.style.cssText = `
       position: fixed;
       top: 70px;
